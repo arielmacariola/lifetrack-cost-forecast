eb4609b18cd353d58394b6c223a72f37ef4fa6f3
arielmac <arielmacariola.profs@yahoo.com>
2020-02-15 01:16:11 +0800

    Move favicons in images folder

--- webroot/index.php
+++ webroot/index.php
@@ -17,8 +17,8 @@
     <link rel="stylesheet" href="http://<?php echo $_SERVER[ 'HTTP_HOST' ] ?>/css/style.css">
     <script src="http://<?php echo $_SERVER[ 'HTTP_HOST' ] ?>/js/script.js"></script>
 
-    <link rel="icon" href="http://lifetrackmed.com/wp-content/uploads/2018/07/LMSlogo-fav16.png" type="image/png" sizes="16x16">
-    <link rel="icon" href="http://lifetrackmed.com/wp-content/uploads/2018/07/LMSlogo-fav32.png" type="image/png" sizes="32x32">
+    <link rel="icon" href="images/LMSlogo-fav16.png" type="image/png" sizes="16x16">
+    <link rel="icon" href="images/LMSlogo-fav32.png" type="image/png" sizes="32x32">
     
     <title>Lifetrack - Infra Cost Forecasting</title>
 </head>
@@ -33,15 +33,18 @@
             <form id="formForcast">
                 <div class="form-group">
                     <label for="totalStudyPerDay">Number of study per day</label>
-                    <input type="number" class="form-control" id="numOfStudyPerDay" min="0" required>
+                    <input type="number" class="form-control" id="numOfStudyPerDay" min="1" max="100000000" value="1000" required>
+                    <small id="totalStudyPerDayHelp" class="form-text text-muted">The value must be minimum 1 and maximum 100,000,000</small>
                 </div>
                 <div class="form-group">
                     <label for="numOfStudyGrowthPerMonth">Study growth per month in %</label>
-                    <input type="number" class="form-control" id="numOfStudyGrowthPerMonth" min="0" required>
+                    <input type="number" class="form-control" id="numOfStudyGrowthPerMonth" min="0" max="200" value="1" required>
+                    <small id="numOfStudyGrowthPerMonthHelp" class="form-text text-muted">The value must be minimum 0 and maximum 200</small>
                 </div>
                 <div class="form-group">
                     <label for="numOfMonthsForecast">Number of months to forecast</label>
-                    <input type="number" class="form-control" id="numOfMonthsForecast" min="0" required>
+                    <input type="number" class="form-control" id="numOfMonthsForecast" min="1" max="120" value="5" required>
+                    <small id="numOfMonthsForecastHelp" class="form-text text-muted">The value must be minimum 1 and maximum 120 (10 years)</small>
                 </div>
                 <button type="submit" class="btn btn-info" id="submitBtn" >Submit</button>
             </form>
@@ -49,21 +52,23 @@
         <div class="container">
             <div id="result">
                 <h3 class="sub-title">Forecast Data Result</h3>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Month Year</th>
-                            <th scope="col">Number of Studies</th>
-                            <th scope="col">Cost Forecasted</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td colspan="4" align="center">Please complete the form above then submit.</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Month Year</th>
+                                <th scope="col">Number of Studies</th>
+                                <th scope="col">Cost Forecasted</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td colspan="4" align="center">Please complete the form above then submit.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
         <footer class="container">
